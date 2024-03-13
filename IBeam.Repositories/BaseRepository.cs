08f357f694fba6bb60b61929e89875ddaf88b42e
mp-abram-cookson <abram.cookson@medpactech.com>
2024-03-13 19:23:10 -0400

    Added new base service

--- IBeam.Repositories/BaseRepository.cs
+++ IBeam.Repositories/BaseRepository.cs
@@ -135,6 +135,37 @@ namespace IBeam.Repositories
 
         }
 
+        public bool Archive(T dto)
+        {
+            if(dto is IDTOArchive dtoArchive)
+            {
+                dtoArchive.IsArchived = true;
+                Save(dto);
+                return true;
+            }
+            else
+            {
+                throw new Exception("DTO does not implement IDTOArchive");
+            }
+        }
+
+        public void ArchiveAll(List<T> dtos)
+        {
+
+            dtos.ForEach(dto =>
+            {
+                if (dto is IDTOArchive dtoArchive)
+                {
+                    dtoArchive.IsArchived = true;
+                }
+                else
+                {
+                    throw new Exception("DTOs do not implement IDTOArchive");
+                }
+            });
+            SaveAll(dtos);
+        }
+
         public void Delete(T dto)
         {
             try
