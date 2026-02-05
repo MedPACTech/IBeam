5ef4be8a79ad458fe39a13a6d244e05ec832f9a1
mp-abram-cookson <abram.cookson@medpactech.com>
2026-02-05 13:30:57 -0500

    Added changes to baseService and Added Automapper
    We may move all Service Implementations to IBeam.Services.Scaffolding.

---
+++ IBeam.Services.AutoMapper/AutoMapperModel.cs
@@ -0,0 +1,20 @@
+﻿using IBeam.Services.Abstractions;
+
+public sealed class AutoMapperModelMapper<TEntity, TModel> : IModelMapper<TEntity, TModel>
+    where TEntity : class
+    where TModel : class
+{
+    private readonly AutoMapper.IMapper _mapper;
+
+    public AutoMapperModelMapper(AutoMapper.IMapper mapper)
+        => _mapper = mapper;
+
+    public TEntity ToEntity(TModel model) => _mapper.Map<TEntity>(model);
+    public TModel ToModel(TEntity entity) => _mapper.Map<TModel>(entity);
+
+    public IEnumerable<TEntity> ToEntity(IEnumerable<TModel> models)
+        => models.Select(ToEntity);
+
+    public IEnumerable<TModel> ToModel(IEnumerable<TEntity> entities)
+        => entities.Select(ToModel);
+}
