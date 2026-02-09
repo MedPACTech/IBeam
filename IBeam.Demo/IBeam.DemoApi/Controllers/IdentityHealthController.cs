6c3ae5847fd219603161863927fda2ba39b60071
mp-christian-armstrong <christian.armstrong@medpactech.com>
2026-02-09 15:42:16 -0500

    Demo App with good build and creating Identity Tables.

---
+++ IBeam.Demo/IBeam.DemoApi/Controllers/IdentityHealthController.cs
@@ -0,0 +1,13 @@
+using IBeam.Identity.Core.Auth.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IBeam.DemoApi.Controllers;
+
+[ApiController]
+[Route("api/identity")]
+public sealed class IdentityHealthController : ControllerBase
+{
+    [HttpGet("ping")]
+    public IActionResult Ping([FromServices] IAuthService authService)
+        => Ok(new { ok = true, authService = authService.GetType().FullName });
+}
