b9c8397864136e87dfeb59f37617952e67232dd8
mp-abram-cookson <abram.cookson@medpactech.com>
2026-02-17 16:20:10 -0500

    Email Templating Complete

--- IBeam.Communications.Abstractions/Interfaces/ISmsService.cs
+++ IBeam.Communications.Abstractions/Interfaces/ISmsService.cs
@@ -1,8 +1,22 @@
 namespace IBeam.Communications.Abstractions;
 
-
 public interface ISmsService
 {
     Task SendAsync(SmsMessage message, SmsSendOptions? options = null, CancellationToken ct = default);
+
+    async Task SendAsync(
+        string to,
+        string body,
+        SmsSendOptions? options = null,
+        CancellationToken ct = default)
+    {
+        var message = new SmsMessage
+        {
+            Body = body
+        };
+        message.To.Add(to);
+
+        await SendAsync(message, options, ct);
+    }
 }
 
