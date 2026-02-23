a7f68cc9b0ec10f5ac9328f7c3e51e04bdad9ef6
mp-abram-cookson <abram.cookson@medpactech.com>
2026-02-23 11:04:13 -0500

    Added Sms to OTP

---
+++ IBeam.Identity.Abstractions/Interfaces/IOtpSender.cs
@@ -0,0 +1,14 @@
+﻿namespace IBeam.Identity.Abstractions.Interfaces;
+
+using IBeam.Identity.Abstractions.Models;
+
+public interface IOtpSender
+{
+    Task SendAsync(
+        OtpChannel channel,
+        string destination,
+        string code,
+        OtpPurpose purpose,
+        Guid? tenantId,
+        CancellationToken ct = default);
+}
---
+++ IBeam.Identity.Abstractions/Models/OtpChannel.cs
@@ -0,0 +1,7 @@
+﻿namespace IBeam.Identity.Abstractions.Models;
+
+public enum OtpChannel
+{
+    Email = 1,
+    Sms = 2
+}
