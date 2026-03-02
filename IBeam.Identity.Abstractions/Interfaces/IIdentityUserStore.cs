fed248837a217a5a17bf4dfaebff3b132429f284
mp-abram-cookson <abram.cookson@medpactech.com>
2026-03-02 08:39:45 -0500

    Email and Password login available

--- IBeam.Identity.Abstractions/Interfaces/IIdentityUserStore.cs
+++ IBeam.Identity.Abstractions/Interfaces/IIdentityUserStore.cs
@@ -10,6 +10,8 @@ public interface IIdentityUserStore
 
     Task<CreateUserResult> CreateAsync(RegisterUserRequest request, CancellationToken ct = default);
     Task<bool> ValidatePasswordAsync(string emailOrPhone, string password, CancellationToken ct = default);
+    Task SetPasswordAsync(Guid userId, string newPassword, CancellationToken ct = default);
+    Task SetEmailConfirmedAsync(Guid userId, bool confirmed, CancellationToken ct = default);
 
     Task UpdateEmailAsync(Guid userId, string newEmail, CancellationToken ct = default);
     Task UpdatePhoneAsync(Guid userId, string newPhone, CancellationToken ct = default);
